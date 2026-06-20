5f543914971f21937e3edb07efa4f3b6dae8e348
nikhil kumar <nikhilkmr1725@gmail.com>
2026-06-21 00:44:35 +0530

    partial half done

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,8 @@ require ("dotenv").config();
 const express=require("express");
 const cors=require("cors");
 const path=require("path");
-
+const connectDB=require("./config/db");
+const authRoutes = require("./routes/authRoutes");
 const app=express();
 
 //Middleware to handle Cors
@@ -14,10 +15,18 @@ app.use(
     })
 );
 
+connectDB()
+
 //Middleware
 app.use(express.json());
 
 //Routes
+app.use("/api/auth",authRoutes);
+// app.use('/api/sessions',sessionRoutes);
+// app.use('/api/questions',questionRoutes);
+// app.use('/api/ai/generte-questions',Project, generateInterviewQuestions);
+// app.use('/api/ai/generate-explanation',project,generateConceptExplanation);
+
 
 //Serve uploads folder
 app.use("/uploads",express.static(path.join(__dirname,"uploads"),{}));
